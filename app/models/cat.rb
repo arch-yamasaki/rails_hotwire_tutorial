69ed9dae4e3e98b79efd5c233542a0dea15c102c
arch-yamasaki <yohei7328@gmail.com>
2023-02-09 17:59:52 +0900

    validationを追加

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,2 +1,5 @@
 class Cat < ApplicationRecord
+  validates :name, presence: true
+  # required, greater than 0, integer
+  validates :age, presence: true, numericality: { greater_than_or_equal_to: 0, only_integer: true }
 end
